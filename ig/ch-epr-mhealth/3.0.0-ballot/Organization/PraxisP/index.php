ab5dfd3e707cc9ddffbd0f85b54fd8984a8e97f2
oliveregger <oliver.egger@ahdis.ch>
2023-06-30 16:11:53 +0200

    ch-epr-term ballot

---
+++ ig/ch-epr-mhealth/3.0.0-ballot/Organization/PraxisP/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/Organization-PraxisP.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/Organization-PraxisP.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/Organization-PraxisP.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/Organization-PraxisP.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/Organization-PraxisP.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/Organization-PraxisP.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/Organization-PraxisP.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ ig/ch-epr-mhealth/CapabilityStatement/CH.mCSD.CareServicesSelectiveSupplier/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/CapabilityStatement-CH.mCSD.CareServicesSelectiveSupplier.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/CapabilityStatement-CH.mCSD.CareServicesSelectiveSupplier.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/CapabilityStatement-CH.mCSD.CareServicesSelectiveSupplier.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/CapabilityStatement-CH.mCSD.CareServicesSelectiveSupplier.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/CapabilityStatement-CH.mCSD.CareServicesSelectiveSupplier.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/CapabilityStatement-CH.mCSD.CareServicesSelectiveSupplier.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0-ballot/CapabilityStatement-CH.mCSD.CareServicesSelectiveSupplier.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
