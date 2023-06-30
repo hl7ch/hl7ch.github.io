8d810846cdcc83a9a8eec0fc20d3e1f5066543dc
oliveregger <oliver.egger@ahdis.ch>
2023-06-30 09:49:29 +0200

    ch-atc 3.2.0-ballot

---
+++ ig/ch-atc/3.2.0-ballot/AuditEvent/atc-doc-read-ass-hpc/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/AuditEvent-atc-doc-read-ass-hpc.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/AuditEvent-atc-doc-read-ass-hpc.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/AuditEvent-atc-doc-read-ass-hpc.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/AuditEvent-atc-doc-read-ass-hpc.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/AuditEvent-atc-doc-read-ass-hpc.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/AuditEvent-atc-doc-read-ass-hpc.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/AuditEvent-atc-doc-read-ass-hpc.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ ig/ch-atc/StructureDefinition/DocumentAuditEvent/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/StructureDefinition-DocumentAuditEvent.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/StructureDefinition-DocumentAuditEvent.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/StructureDefinition-DocumentAuditEvent.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/StructureDefinition-DocumentAuditEvent.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/StructureDefinition-DocumentAuditEvent.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/StructureDefinition-DocumentAuditEvent.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-atc/3.2.0-ballot/StructureDefinition-DocumentAuditEvent.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
