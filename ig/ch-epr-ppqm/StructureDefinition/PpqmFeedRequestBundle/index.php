5c878a08497e97aea65d49c5e56bf493325515d9
Michaela Ziegler <michaela.ziegler@ahdis.ch>
2023-12-20 14:20:52 +0100

    ch-epr-ppqm 2.0.0

---
+++ ig/ch-epr-ppqm/StructureDefinition/PpqmFeedRequestBundle/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-ppqm/2.0.0/StructureDefinition-PpqmFeedRequestBundle.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-ppqm/2.0.0/StructureDefinition-PpqmFeedRequestBundle.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-ppqm/2.0.0/StructureDefinition-PpqmFeedRequestBundle.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-ppqm/2.0.0/StructureDefinition-PpqmFeedRequestBundle.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-ppqm/2.0.0/StructureDefinition-PpqmFeedRequestBundle.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-ppqm/2.0.0/StructureDefinition-PpqmFeedRequestBundle.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-epr-ppqm/2.0.0/StructureDefinition-PpqmFeedRequestBundle.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
