c5a042c595e3efaa05310b0ee8d79aeafaa573ea
oliveregger <oliver.egger@ahdis.ch>
2024-01-31 13:22:51 +0100

    ch-elm 1.1.0

---
+++ ig/ch-elm/1.1.0/ServiceRequest/1bSR-Order/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.0/ServiceRequest-1bSR-Order.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.0/ServiceRequest-1bSR-Order.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.0/ServiceRequest-1bSR-Order.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.0/ServiceRequest-1bSR-Order.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.0/ServiceRequest-1bSR-Order.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.0/ServiceRequest-1bSR-Order.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.0/ServiceRequest-1bSR-Order.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
