95458d768f65eb408cfcea99e94047d8b53a3c98
oliveregger <oliver.egger@ahdis.ch>
2024-02-27 13:38:06 +0100

    ch-elm with version 1.1.1

---
+++ ig/ch-elm/ValueSet/ch-elm-interpretation-codes-pos-neg/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.1/ValueSet-ch-elm-interpretation-codes-pos-neg.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.1/ValueSet-ch-elm-interpretation-codes-pos-neg.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.1/ValueSet-ch-elm-interpretation-codes-pos-neg.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.1/ValueSet-ch-elm-interpretation-codes-pos-neg.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.1/ValueSet-ch-elm-interpretation-codes-pos-neg.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.1/ValueSet-ch-elm-interpretation-codes-pos-neg.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-elm/1.1.1/ValueSet-ch-elm-interpretation-codes-pos-neg.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
