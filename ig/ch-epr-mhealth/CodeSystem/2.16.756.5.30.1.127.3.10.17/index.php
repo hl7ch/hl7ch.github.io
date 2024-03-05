7645f3b4db30d8097de4f66f404b25af01bf89d5
oliveregger <oliver.egger@ahdis.ch>
2024-03-05 12:16:13 +0100

    ch epr mhealth published version 3.0.0 on 2024-03-05

---
+++ ig/ch-epr-mhealth/CodeSystem/2.16.756.5.30.1.127.3.10.17/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0/CodeSystem-2.16.756.5.30.1.127.3.10.17.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0/CodeSystem-2.16.756.5.30.1.127.3.10.17.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0/CodeSystem-2.16.756.5.30.1.127.3.10.17.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0/CodeSystem-2.16.756.5.30.1.127.3.10.17.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0/CodeSystem-2.16.756.5.30.1.127.3.10.17.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0/CodeSystem-2.16.756.5.30.1.127.3.10.17.html');
+else 
+  Redirect('http://fhir.ch/ig/ch-epr-mhealth/3.0.0/CodeSystem-2.16.756.5.30.1.127.3.10.17.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
